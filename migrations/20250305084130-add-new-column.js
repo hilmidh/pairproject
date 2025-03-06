c9af59d71e936568b6d39cfd559fee5e8ba74ee8
Hilmi Ahmad <hilmiadd@gmail.com>
2025-03-06 12:29:38 +0700

    routing jadi semua

--- migrations/20250305084130-add-new-column.js
+++ migrations/20250305084130-add-new-column.js
@@ -9,7 +9,7 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    await queryInterface.addColumn("Profiles", "fullName", Sequelize.INTEGER)
+    await queryInterface.addColumn("Profiles", "fullName", Sequelize.STRING)
   },
 
   async down (queryInterface, Sequelize) {
